c94e4e2847ae8a70a37bef13f3a3af29c316a027
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2024-12-22 01:42:47 +0800

    src/consumer/csvconsumer.py: sort data by time
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- src/consumer/csvconsumer.py
+++ src/consumer/csvconsumer.py
@@ -4,5 +4,7 @@ class CSVConsumer():
         self.data = pd.read_csv(file_path)
         if "Timestamp" in self.data.columns:
             self.data["Timestamp"] = pd.to_datetime(self.data["Timestamp"]).astype('int64') // 1e9
+            self.data.sort_values(by="Timestamp", inplace=True)
         else:
             self.data["TimeUnix"] = pd.to_datetime(self.data["TimeUnix"]).astype('int64') // 1e9
+            self.data.sort_values(by="TimeUnix", inplace=True)
